3837094cbf74718b504a5c7d47ca33dd57d0fbe6
Vinicius Fagner da Silva <133381911+terracota6@users.noreply.github.com>
2024-09-23 10:38:10 -0300

    23/09/24

---
+++ main.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Lucas Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-peso");
+var altura = tdAltura.textContent;
